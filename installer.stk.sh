f0aaf50691a44e1704d28ed0833600b608cc7593
Bas Nelissen <b.g.l.nelissen@gmail.com>
2014-10-24 21:24:37 +0200

    add install script

---
+++ installer.stk.sh
@@ -0,0 +1,6 @@
+sudo apt-get ssh
+# ubuntu installatie cellprofiler
+git clone https://github.com/CellProfiler/CellProfiler.git
+
+# hosted packages http://cellprofiler.org/linked_files/CPPackageHost/
+# sudo apt-get install default-jre default-jdk
